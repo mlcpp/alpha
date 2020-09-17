e70cafadf80204f9775e98f55ea3c0c24a76c822
Ashok Arora <arora8715@gmail.com>
2020-09-17 14:32:28 +0530

    Provides optimal folder structure and clang-format
    Folder structure is as follows:
        -- src/ for header files
        -- examples/ for implementation of header use cases
        -- build/ for CMake output
        -- datasets/ for storing dataset csv files with description
    
    clang-format can be tweaked and run on files after the file has been
    written. It will probably be run in conjecture with CMake.

---
+++ src/LinearRegression.hpp
@@ -0,0 +1,15 @@
+#ifndef _linear_regression_hpp_
+#define _linear_regression_hpp_
+
+template <typename T>
+class LinearRegression{
+  private:
+  public:
+  void fit();
+  void get_params();
+  void predict();
+  void score();
+  void set_params();
+};
+
+#endif /* _linear_regression_hpp_ */
