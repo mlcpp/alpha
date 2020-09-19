a4613ad428e449e2417daac81fb4ef2fc0b2fd53
Ashok Arora <arora8715@gmail.com>
2020-09-19 12:32:16 +0530

    Adds all.h

--- examples/graph_template.cpp
+++ examples/graph_template.cpp
@@ -2,14 +2,13 @@
 #include <iostream>
 #include <matplotlibcpp.hpp>
 #include <vector>
-using namespace std;
-namespace plt = matplotlibcpp;
+
 int main() {
-    vector<int> x, y;
+    std::vector<int> x, y;
     for (int i = 1; i < 30; i++) {
         y.push_back(rand() % 100);
         x.push_back(i);
     }
-    plt::plot(x, y);
-    plt::show();
+    matplotlibcpp::plot(x, y);
+    matplotlibcpp::show();
 }
