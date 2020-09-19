dfa8dc5c8885697d62b6fce4a5dc2f1cdf9b1ad0
Ashok Arora <arora8715@gmail.com>
2020-09-19 12:16:21 +0530

    Fixes graph template and Makefile issues

--- examples/graph_template.cpp
+++ examples/graph_template.cpp
@@ -1,15 +1,15 @@
-#include "../src/matplotlibcpp.hpp"
-#include <iostream>
-#include <vector>
 #include <cstdlib>
+#include <iostream>
+#include <matplotlibcpp.hpp>
+#include <vector>
 using namespace std;
 namespace plt = matplotlibcpp;
 int main() {
-    vector <int> x, y;
-    for ( int i = 1 ; i < 30 ; i++ ){
-        y.push_back(rand()%100);
+    vector<int> x, y;
+    for (int i = 1; i < 30; i++) {
+        y.push_back(rand() % 100);
         x.push_back(i);
-    }  
+    }
     plt::plot(x, y);
-    plt::show();  
+    plt::show();
 }
