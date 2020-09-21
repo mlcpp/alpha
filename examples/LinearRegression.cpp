5a5a973f6a025089de29b02f0486a6d8b8849757
Ashok Arora <arora8715@gmail.com>
2020-09-21 15:09:01 +0530

    Example usecase for Linear Regression

--- examples/LinearRegression.cpp
+++ examples/LinearRegression.cpp
@@ -1,10 +1,59 @@
 // Header files
-// Load the datasets
-// Use only one feature
-// Split the data into training/testing sets
-// Split the targets into training/testing sets
-// Create linear regression object
-// Train the model using the training set
-// Make prediction using the testing set
-// Print coefficients, mean squared error and coefficient of determination
-// Plot outputs
+#include <LinearRegression.hpp>
+using mathplotlibcpp as plt;
+
+int main(){
+
+    // Load the datasets
+    Matrix mat = read_csv("./datasets/boston/diabetes.csv");
+
+    // Use only one feature
+    mat = mat.slice();
+
+    // Split the data into training/testing sets
+    pair <Matrix, Matrix> X = split_test_train(mat);
+    Matrix X_train = X.first;
+    Matrix X_test = X.second;
+
+    // Split the targets into training/testing sets
+    pair <Matrix, Matrix> Y = split_test_train(mat);
+    Matrix Y_train = Y.first;
+    Matrix Y_test = Y.second;
+
+    // Create linear regression object
+    LinearRegression regr{};
+
+    // Train the model using the training set
+    regr.fit(X_train, Y_train);
+
+    // Make prediction using the testing set
+    Matrix Y_pred = regr.predict(X_test);
+
+    // Print coefficients
+    Matrix coef = regr.coef_;
+    coef.view();
+
+    // The mean squared error
+    std :: cout << mean_squared_error(diabetes_y_test, diabetes_y_pred);
+
+    // The coefficient of determination: 1 is perfect prediction
+    std :: cout << r2_score(diabetes_y_test, diabetes_y_pred);
+
+    // Plot outputs
+    plt::figure_size(1200, 780);
+    plt::scatter(X_test, Y_test);
+    plt::plot(X_test, Y_pred);
+    plt::named_plot("Linear regression", x, y);
+    // Set x-axis to interval [0,1000000]
+    plt::xlim(0, 1000*1000);
+    // Add graph title
+    plt::title("Sample figure");
+    // Enable legend.
+    plt::legend();
+    // Save the image (file format is determined by the extension)
+    plt::save("./basic.png");
+
+    matplotlibcpp::show();
+
+    return 0;
+}
