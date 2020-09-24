1cde38735a6c94cd7e872633e3513b7e0767aac8
Aniket Sharma <aniketsharma00411@gmail.com>
2020-09-24 16:25:31 +0530

    Ridge Regression

---
+++ include/Ridge.hpp
@@ -0,0 +1,108 @@
+#ifndef _ridge_hpp_
+#define _ridge_hpp_
+
+#include <all.hpp>
+
+class Ridge {
+  private:
+    bool normalize, if_fit = false;
+    int epochs;
+    double lr, alpha;
+
+  public:
+    Matrix B;
+
+    Ridge(double, bool, int, double);
+    Matrix fit(Matrix, Matrix);
+    void get_params();
+    Matrix predict(Matrix);
+    double score(Matrix, Matrix);
+    void set_params(double, bool, int, double);
+};
+
+// Constructor
+Ridge::Ridge(double alpha = 1, bool normalize = false, int epochs = 1000, double lr = 0.001) {
+    this->alpha = alpha;
+    this->normalize = normalize;
+    this->epochs = epochs;
+    this->lr = lr;
+}
+
+// Method to fit the Ridge model
+Matrix Ridge::fit(Matrix X, Matrix Y) {
+
+    if ((X.row_length() != Y.row_length()) && (X.col_length() == Y.col_length())) {
+        X.T();
+        Y.T();
+    }
+
+    bool expr = (X.row_length() == Y.row_length()) && (X.col_length() == Y.col_length());
+    assert(("Wrong dimensions.", expr));
+
+    // Initializing parameters with zero
+    B = matrix.zeros(X.col_length() + 1, 1);
+
+    // Add a column of 1's to X
+    Matrix temp_x = matrix.ones(X.row_length(), 1);
+    X = matrix.concat(temp_x, X, "column");
+    int m = X.row_length();
+    Matrix temp;
+    Matrix Y_pred;
+
+    // gradient descent
+    for (int i = 1; i <= epochs; i++) {
+        temp = matrix.concat(B.slice(0, B.row_length(), 0, 1),
+                             matrix.zeros(B.row_length(), B.col_length() - 1), "row");
+        Y_pred = matrix.matmul(X, B);
+        B = B - ((matrix.matmul(X.T(), Y_pred - Y) + (B * alpha)) * (lr / m));
+        B = B + (temp * alpha);
+    }
+    if_fit = true;
+
+    return B;
+}
+
+// Method to print the Ridge object parameters in json format
+void Ridge::get_params() {
+    std::cout << std::boolalpha;
+    std::vector<std::string> params = {"alpha", "normalize", "epochs", "lr"};
+    std::cout << "[" << std::endl;
+    std::cout << "\t \"alpha\": \"" << this->alpha << "\"," << std::endl;
+    std::cout << "\t \"normalize\": \"" << this->normalize << "\"," << std::endl;
+    std::cout << "\t \"epochs\": \"" << this->epochs << "\"," << std::endl;
+    std::cout << "\t \"lr\": \"" << this->lr << "\"" << std::endl;
+    std::cout << "]" << std::endl;
+}
+
+// Method to predict using the Ridge model
+Matrix Ridge::predict(Matrix X) {
+    assert(("Fit the model before predicting.", if_fit));
+
+    // Add a column of 1's to X
+    Matrix temp_x = matrix.ones(X.row_length(), 1);
+    X = matrix.concat(temp_x, X, "column");
+    Matrix Y_pred;
+    Y_pred = matrix.matmul(X, B);
+    return Y_pred;
+}
+
+// Method to calculate the score of the predictions
+double Ridge::score(Matrix Y_pred, Matrix Y) {
+    double score = 0.00;
+    Matrix residual_sum_of_squares = matrix.matmul((Y_pred - Y).T(), (Y_pred - Y));
+    Matrix total_sum_of_squares = matrix.matmul((Y - (matrix.mean(Y, "column"))(0, 0)).T(),
+                                                (Y - (matrix.mean(Y, "column"))(0, 0)));
+    score = (1 - (residual_sum_of_squares(0, 0) / total_sum_of_squares(0, 0)));
+    return score;
+}
+
+// Method to set the Ridge object parameters
+void Ridge::set_params(double alpha = 1, bool normalize = false, int epochs = 1000,
+                       double lr = 0.001) {
+    this->alpha = alpha;
+    this->normalize = normalize;
+    this->epochs = epochs;
+    this->lr = lr;
+}
+
+#endif /* _ridge_hpp_ */
