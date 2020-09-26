cbd977fc823d71de8aeb7c599bd72067424c4986
Ashok Arora <arora8715@gmail.com>
2020-09-26 13:47:16 +0530

    Fixed merge conflict

--- examples/tuple_unpack.cpp
+++ examples/tuple_unpack.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-tuple<int, int, int, int> ok(int a, int b) { return {a/2, b/2, a, b}; }
+tuple<int, int, int, int> ok(int a, int b) { return {a / 2, b / 2, a, b}; }
 
 int main() {
     auto [value1, value2, value3, value4] = ok(1, 2);
--- include/KMeans.hpp
+++ include/KMeans.hpp
@@ -6,6 +6,8 @@
 class KMeans {
   private:
     int n_clusters, epochs;
+    Matrix centroid_selection(Matrix, int);
+    Matrix distance(Matrix, Matrix);
 
   public:
     KMeans(int n_clusters, int epochs) {
@@ -13,27 +15,61 @@ class KMeans {
         this->epochs = epochs;
     }
     // computes k optimal centroids and classifies given X points
-    Matrix fit(Matrix X);
+    void fit(Matrix);
 
     // compute k optimal centroids and classifies each new points into k clusters
-    Matrix fit_predict(Matrix X);
+    Matrix fit_predict(Matrix);
 
-    Matrix fit_transform(Matrix X);
+    Matrix fit_transform(Matrix);
     void get_params();
 
     // classifies new given points into one of ready k clusters
-    Matrix predict(Matrix X);
-    double score(Matrix X);
+    Matrix predict(Matrix);
+    double score(Matrix);
     void set_params();
-    Matrix predict(Matrix X);
-    Matrix transform(Matrix X);
+    Matrix predict(Matrix);
+    Matrix transform(Matrix);
 };
 
-Matrix KMeans::fit(Matrix X) {
+void KMeans::centroid_selection(Matrix X, int k) {
+    std::vector<std::vector<double>> temp_vec;
+    srand(time(NULL));
+
+    for (int i = 0; i < k; i++) {
+        temp_vec.push_back(X.get_row(rand() % X.row_length()));
+    }
+    return matrix.init(temp_vec);
+}
+
+void KMeans::distance(Matrix X, Matrix Y){
+  // check their dimensions are same
+  return matrix.sqrt(matrix.power(X-Y, 2));
+}
+
+void KMeans::fit(Matrix X) {
     /*
      * input mxn matrix
-     * return matrix Y where Y[i][j] = 1 if j belongs to cluster i
+     * return matrix Z where Z[i][j] = 1 if j belongs to cluster i
+     * either run for epochs, or stop at convergence, ie z - z' = 0
      */
+
+    Matrix C = centroid_selection(X, k);
+    Matrix Z = matrix.zeros(X.row_length(), k);
+
+    Matrix Y = Y - matrix.matmul(( X - matrix.matmul(Z.T(), C), ( X - matrix.matmul(Z.T(), C).T());
+  	Matrix C = matrix.matmul(matrix.matmul(X, Z.T()), matrix.inverse(matrix.matmul(Z, Z.T())))
+	
+  	Matrix Z = matrix.zeros(X.row_length(), 1);
+  	for ( int i = 0 ; i < epochs ; i++ ){
+        Matrix temp = distance(X, C);         // (m,k)
+        Matrix z = matrix.argmin(temp, "row") // (m,1)
+                   C = update_mean(X, C, Z);
+	}
 }
+
+Matrix<>::update_mean() {
+    for (int j = 0; j < k; j++) {
+        C = update_mean(all x where x belongs to ci) // (k, n)
+    }
 
 #endif /* _k_means_hpp_ */
